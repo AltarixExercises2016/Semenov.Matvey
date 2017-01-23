f4658019fc0fe7356ef8a56acac3ba2f04bb7e53
matek3022 <matek3022@gmail.com>
2017-01-23 21:40:27 +0400

    add all

---
+++ VKMessager/app/src/main/java/com/example/vk_mess_demo_00001/activitys/SettingActivity.java
@@ -0,0 +1,79 @@
+package com.example.vk_mess_demo_00001.activitys;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import com.example.vk_mess_demo_00001.R;
+import com.example.vk_mess_demo_00001.managers.PreferencesManager;
+
+public class SettingActivity extends AppCompatActivity {
+    PreferencesManager preferencesManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(getString(R.string.SETTINGS));
+        setContentView(R.layout.activity_setting);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Switch photouser = (Switch) findViewById(R.id.switch1);
+        Switch photochat = (Switch) findViewById(R.id.switch3);
+        Switch online = (Switch) findViewById(R.id.switch2);
+        Button button = (Button) findViewById(R.id.button);
+        preferencesManager = PreferencesManager.getInstance();
+
+
+        photouser.setChecked(preferencesManager.getSettingPhotoUserOn());
+        photochat.setChecked(preferencesManager.getSettingPhotoChatOn());
+        online.setChecked(preferencesManager.getSettingOnline());
+
+
+        photouser.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                preferencesManager.setSettingPhotoUserOn(isChecked);
+            }
+        });
+        photochat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                preferencesManager.setSettingPhotoChatOn(isChecked);
+            }
+        });
+        online.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                preferencesManager.setSettingOnline(isChecked);
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                preferencesManager.setToken("");
+                startActivity(StartActivity.getIntent(SettingActivity.this,true,true));
+                SettingActivity.this.finish();
+            }
+        });
+    }
+
+    static Intent getIntent (Context context){
+        Intent intent = new Intent(context, SettingActivity.class);
+        return intent;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ VKMessager/app/src/main/java/com/example/vk_mess_demo_00001/vkobjects/Attachment.java
@@ -0,0 +1,49 @@
+package com.example.vk_mess_demo_00001.vkobjects;
+
+import com.example.vk_mess_demo_00001.vkobjects.attachmenttype.AudioMess;
+import com.example.vk_mess_demo_00001.vkobjects.attachmenttype.DocMess;
+import com.example.vk_mess_demo_00001.vkobjects.attachmenttype.PhotoMess;
+import com.example.vk_mess_demo_00001.vkobjects.attachmenttype.VideoMess;
+import com.example.vk_mess_demo_00001.utils.Link;
+
+public class Attachment {
+
+    private String type;
+    private PhotoMess photo;
+    private PhotoMess sticker;
+    private PhotoMess gift;
+    private VideoMess video;
+    private Link link;
+    private DocMess doc;
+    private AudioMess audio;
+    public String getType() {
+        return type;
+    }
+    public PhotoMess getPhoto() {
+        return photo;
+    }
+
+    public PhotoMess getGift() {
+        return gift;
+    }
+
+    public PhotoMess getSticker() {
+        return sticker;
+    }
+
+    public VideoMess getVideo() {
+        return video;
+    }
+
+    public Link getLink() {
+        return link;
+    }
+
+    public DocMess getDoc() {
+        return doc;
+    }
+
+    public AudioMess getAudio() {
+        return audio;
+    }
+}
